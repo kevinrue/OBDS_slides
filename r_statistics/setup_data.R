49e43a161a2850efd67fba273d5dec468d0b323b
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-04-30 17:00:08 +0100

    Update slides, exercises and solution for R stats lesson (#45)
    * update date and prerequisites
    
    * update renv
    
    * update ecdf exercise; add a slide to illustrate code and result for fisher's test
    
    * redraw illustrations myself
    
    * updated second slide illustrating fisher's test; need to finish the combinatorial formula though
    
    * update renv; outline the approach of exercises; start updating the solution code
    
    * update script to preprocess data
    
    * ignore backup of data
    
    * update exercises and solution
    
    * fixes
    
    * rename exercises to solution
    
    * rename exercises to solution

--- r_statistics/setup_data.R
+++ r_statistics/setup_data.R
@@ -1,57 +1,77 @@
 library(tidyverse)
 library(matrixStats)
-
-# SC ----
-
-# Read full normalised matrix
-mat <- read.csv("../data/aulicino/logcount_matrix.txt", header = TRUE, sep = "\t")
-mat <- as.matrix(mat)
-
-# Re-export gene information
-gene_metadata <- read.csv("../data/aulicino/gene_metadata.txt", header = TRUE, sep = "\t")
-gene_subset_metadata <- gene_metadata[rownames(mat), c("gene_id", "gene_name")]
-write.csv(gene_subset_metadata, "data/gene_metadata.csv", row.names = FALSE, quote = FALSE)
-
-# Read cell metadata
-# Subset cells and metadata
-cell_metadata <- read.csv("../data/aulicino/cell_metadata.txt", header = TRUE, sep = "\t")
-cell_metadata <- cell_metadata[colnames(mat), ]
-cell_subset_metadata <- subset(cell_metadata, Time == "6h" & Infection %in% c("Mock", "STM-LT2") & Status %in% c("Uninfected", "Infected"), c("Sample", "Infection"))
-write.csv(cell_subset_metadata, "data/cell_metadata.csv", row.names = FALSE, quote = FALSE)
-
-mat <- mat[, cell_subset_metadata$Sample]
-
-# Subset genes
-row_order <- head(order(rowVars(mat), decreasing = TRUE), 1000)
-mat <- mat[row_order, ]
-write.csv(mat, "data/logcounts.csv", quote = FALSE)
-
-# GO:gene mapping ----
-
 library(biomaRt)
+library(GO.db)
+
+# Import source data ----
+
+## Read full normalised matrix ----
+
+logcount_matrix <- read.csv("../data/aulicino/logcount_matrix.txt", header = TRUE, sep = "\t")
+logcount_matrix <- as.matrix(logcount_matrix)
+dim(logcount_matrix)
+
+## Re-export gene information ----
+
+gene_metadata <- read.csv("../data/aulicino/gene_metadata.txt", header = TRUE, sep = "\t")
+dim(gene_metadata)
+
+## Read cell metadata ----
+
+cell_metadata <- read.csv("../data/aulicino/cell_metadata.txt", header = TRUE, sep = "\t")
+dim(cell_metadata)
+
+## Gene sets ----
+
 mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl")
 
 # subset to biological processes associated with genes in the universe
-out <- getBM(attributes = c("ensembl_gene_id", "go_id", "namespace_1003"), filters = "ensembl_gene_id", values = gene_metadata$gene_id, mart = mart)
-out2 <- out %>% 
-  filter(namespace_1003 == "biological_process" & go_id != "") %>% 
-  unique()
+out <- getBM(
+  attributes = c("ensembl_gene_id", "go_id", "namespace_1003"),
+  filters = c("ensembl_gene_id", "go_parent_term"),
+  values = list(gene_metadata_subset$gene_id, "GO:0008150"),
+  mart = mart)
 
-# subset to gene sets with 10 to 100 genes in the universe
-keep_go <- out2 %>% 
+# Subset and reorder data ----
+
+## Gene information ----
+
+gene_metadata_subset <- gene_metadata[rownames(logcount_matrix), c("gene_id", "gene_name")]
+dim(gene_metadata_subset)
+
+## Cell information ----
+
+cell_metadata_subset <- subset(cell_metadata,
+  Time == "6h" &
+    Infection %in% c("Mock", "STM-LT2") &
+    Status %in% c("Uninfected", "Infected") &
+    Sample %in% colnames(logcount_matrix),
+  select = c("Sample", "Infection"))
+dim(cell_metadata_subset)
+
+logcount_matrix_subset <- logcount_matrix[gene_metadata_subset$gene_id, cell_metadata_subset$Sample]
+
+## subset to gene sets with 10 to 100 genes in the universe ----
+keep_go_ids <- out %>% 
   group_by(go_id) %>% 
-  summarise(n = n(), .groups = "keep") %>% 
+  summarise(n = n()) %>% 
   filter(n >= 10 & n <= 100) %>% 
-  ungroup() %>% 
   pull(go_id)
+gene_go_bp_subset <- subset(out, go_id %in% keep_go_ids, c("ensembl_gene_id", "go_id"))
+dim(gene_go_bp_subset)
 
-gene_go_bp_subset <- subset(out2, go_id %in% keep_go, c("ensembl_gene_id", "go_id"))
-write.csv(gene_go_bp_subset, "data/human_go_bp.csv", quote = FALSE, row.names = FALSE)
+## Gene set information ----
 
-# GO definitions ----
-
-library(GO.db)
 # columns(GO.db)
+go_subset <- AnnotationDbi::select(
+  GO.db,
+  keys = unique(gene_go_bp_subset$go_id),
+  columns = c("GOID", "TERM"))
 
-go_subset <- AnnotationDbi::select(GO.db, keys = unique(gene_go_bp_subset$go_id), columns = c("GOID", "TERM"))
+## Export files ----
+
+write.csv(logcount_matrix_subset, "data/logcounts_matrix.csv", quote = FALSE)
+write.csv(cell_metadata_subset, "data/cell_metadata.csv", row.names = FALSE, quote = FALSE)
+write.csv(gene_metadata_subset, "data/gene_metadata.csv", row.names = FALSE, quote = FALSE)
+write.csv(gene_go_bp_subset, "data/human_go_bp.csv", quote = FALSE, row.names = FALSE)
 write.csv(go_subset, "data/go_info.csv", quote = TRUE, row.names = FALSE)
