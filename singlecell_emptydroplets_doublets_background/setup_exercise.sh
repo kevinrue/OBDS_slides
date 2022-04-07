9b757fcaf6acbc800cc8c96a4ac51c88e77272fd
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-04-07 14:36:51 +0100

    update lesson: single cell droplets (#99)
    * rename index file
    
    * update date
    
    * update prereq and setup
    
    * remove git wrapup
    
    * add solution and template
    
    * update exercises about empty droplets and doublets
    
    * bold questions

--- singlecell_emptydroplets_doublets_background/setup_exercise.sh
+++ singlecell_emptydroplets_doublets_background/setup_exercise.sh
@@ -12,6 +12,6 @@ mkdir -p $resource_dir
 
 cd $resource_dir
 
-wget https://cf.10xgenomics.com/samples/cell-exp/3.0.2/5k_pbmc_v3_nextgem/5k_pbmc_v3_nextgem_filtered_feature_bc_matrix.tar.gz
+wget https://cf.10xgenomics.com/samples/cell-exp/3.0.2/5k_pbmc_v3_nextgem/5k_pbmc_v3_nextgem_raw_feature_bc_matrix.tar.gz
 
-tar xvzf 5k_pbmc_v3_nextgem_filtered_feature_bc_matrix.tar.gz
+tar xvzf 5k_pbmc_v3_nextgem_raw_feature_bc_matrix.tar.gz
