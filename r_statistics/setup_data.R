09a09ba0e1ee1168ade258b18b4b03857ef30d48
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-11-13 15:41:38 +0000

    add warmup exercise for statistical tests

--- r_statistics/setup_data.R
+++ r_statistics/setup_data.R
@@ -3,6 +3,23 @@ library(matrixStats)
 library(biomaRt)
 library(GO.db)
 
+# For t-test ----
+
+set.seed(1)
+n_sample_groupA <- 5
+n_sample_groupB <- 5
+mean_groupA <- 2
+sd_groupA <- 0.2
+mean_groupB <- 3
+sd_groupB <- 1
+gene_groupA <- rnorm(n_sample_groupA, mean_groupA, sd_groupA)
+gene_groupB <- rnorm(n_sample_groupB, mean_groupB, sd_groupB)
+df <- data.frame(
+    group = rep(c("groupA", "groupB"), c(n_sample_groupA, n_sample_groupB)),
+    gene_exprs = c(gene_groupA, gene_groupB)
+)
+write.csv(df, "data/gene_exprs.csv", row.names = FALSE, quote = FALSE)
+
 # Import source data ----
 
 ## Read full normalised matrix ----
